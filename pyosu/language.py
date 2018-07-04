a869f81adff522170c295f0ae462da270fcff573
Renondedju <lefantomedelaredstone@gmail.com>
2018-07-04 19:13:29 +0200

    Added language id's

---
+++ pyosu/language.py
@@ -0,0 +1,37 @@
+# MIT License
+
+# Copyright (c) 2018 Renondedju
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+class Language():
+    """ Language id's """
+
+    Any          = 0
+    Other        = 1
+    English      = 2
+    Japanese     = 3
+    Chinese      = 4
+    Instrumental = 5 #Idk you could speak 'Instrumental' :thinking:
+    Korean       = 6
+    French       = 7
+    German       = 8
+    Swedish      = 9
+    Spanish      = 10
+    Italian      = 11
