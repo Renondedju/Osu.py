ab853b4970a829e49432d6bf7e8168c632c687bb
Renondedju <lefantomedelaredstone@gmail.com>
2018-07-10 11:33:52 +0200

    created a base model

---
+++ pyosu/base_model.py
@@ -0,0 +1,49 @@
+# MIT License
+
+# Copyright (c) 2018 Renondedju
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import abc
+
+from .api        import OsuApi
+from .exceptions import UnreferencedApi
+
+class BaseModel(metaclass=abc.ABCMeta):
+    """ This class is just a base model and cannot be instanciated. """
+
+    def __init__(self, api : OsuApi):
+
+        self.__api     = api
+        self.is_empty = True
+
+    @property
+    def api(self):
+        """ api getter """
+
+        if self.__api is None:
+            raise UnreferencedApi("The osu api reference cannot be 'None'")
+        
+        return self.__api
+
+    @api.setter
+    def api(self, value : OsuApi):
+        """ api setter """
+
+        self.__api = value
